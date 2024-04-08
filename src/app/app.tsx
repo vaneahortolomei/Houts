9be20f7409767d84993d1609b7f5c1ac122adcc1
Ваня Хортоломей <hortolomei1989@gmail.com>
2024-04-08 23:12:39 +0300

    :bug: Fixed and refactoring ts configs and other files

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { AppShell, Text } from '@mantine/core';
 
 export const BaseTemplate = () => {
+  const { t } = useTranslation();
   return (
     <>
       <AppShell>
-        <Text>Resize app to see responsive navbar in action</Text>
+        <Text>{t('text')}</Text>
       </AppShell>
     </>
   );
