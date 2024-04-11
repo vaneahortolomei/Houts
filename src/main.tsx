1373e5e40b27e37ccaf5fb7b90af6b7e257c607d
Ваня Хортоломей <hortolomei1989@gmail.com>
2024-04-11 18:43:23 +0300

    :bug: Fixed configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/entities/ix18next/index';
-import { BaseTemplate } from './app/app';
+import { BaseTemplate } from '@/app/app';
 import { MantineProvider } from '@mantine/core';
 
 const rootElement = document.getElementById('root');
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import macrosPlugin from 'vite-plugin-babel-macros';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), macrosPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
