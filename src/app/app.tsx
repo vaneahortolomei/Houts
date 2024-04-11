9cc8209ae91c59e459863221f80e0a7666fa9088
Ваня Хортоломей <hortolomei1989@gmail.com>
2024-04-11 18:34:02 +0300

    :bug: Fixed configs

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { AppShell, Text } from '@mantine/core';
 
 export const BaseTemplate = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   return (
     <>
       <AppShell>
-        <Text>{t('lang.text2')}</Text>
+        <Text>fefe</Text>
       </AppShell>
     </>
   );
