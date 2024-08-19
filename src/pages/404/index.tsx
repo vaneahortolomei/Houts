84062ea488663ee20c0ccb0b0226041b5bd9f454
Ваня Хортоломей <hortolomei1989@gmail.com>
2024-08-19 23:44:34 +0300

    :sparkles: Added 404 and test animation

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ErrorImg from '@/assets/404.png';
+import { Container, Box, Image, Button } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate('/');
+  };
+
+  return (
+    <Container size="xl">
+      <Box
+        style={{
+          backgroundColor: '#fff',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100dvh',
+          flexDirection: 'column'
+        }}
+      >
+        <Image src={ErrorImg} maw={400} h={'auto'} />
+        <Button
+          mt={20}
+          maw={100}
+          w={'100%'}
+          onClick={handleRedirect}
+          variant="filled"
+          color="#A74127"
+          styles={{
+            label: {
+              color: '#fff'
+            }
+          }}
+        >
+          Back
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default NotFound;
--- src/widgets/main/index.tsx
+++ src/widgets/main/index.tsx
@@ -1,7 +1,7 @@
 import { AppShell } from '@mantine/core';
 import { TopSection } from '@/widgets/main/top';
 import { About } from '@/widgets/main/about';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Functionality } from '@/widgets/main/functionality';
 import { BagOptions } from '@/widgets/main/options';
 import { TeaserVideo } from '@/widgets/main/video';
@@ -10,21 +10,64 @@ import { SocialProof } from '@/widgets/main/proof';
 import { Journey } from '@/widgets/main/journey';
 import { Works } from '@/widgets/main/works';
 import { Sustainability } from '@/widgets/main/sustainability';
+import { motion } from 'framer-motion';
+
+interface MotionSectionProps {
+  children: ReactNode;
+}
+
+const sectionVariants = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0 }
+};
+
+const MotionSection: React.FC<MotionSectionProps> = ({ children }) => (
+  <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}
+    transition={{ duration: 0.7, ease: 'easeOut' }}
+    variants={sectionVariants}
+  >
+    {children}
+  </motion.div>
+);
 
 export const Main = () => {
   return (
     <>
       <AppShell.Main style={{ padding: 0 }}>
         <TopSection />
-        <About />
-        <Functionality />
-        <BagOptions />
-        <TeaserVideo />
-        <Reviews />
-        <SocialProof />
-        <Journey />
-        <Works />
-        <Sustainability />
+        <MotionSection>
+          <About />
+        </MotionSection>
+        <MotionSection>
+          <Functionality />
+        </MotionSection>
+        <MotionSection>
+          <BagOptions />
+        </MotionSection>
+        <MotionSection>
+          <TeaserVideo />
+        </MotionSection>
+        <MotionSection>
+          {' '}
+          <Reviews />
+        </MotionSection>
+        <MotionSection>
+          <SocialProof />
+        </MotionSection>
+        <MotionSection>
+          <Journey />
+        </MotionSection>
+        <MotionSection>
+          {' '}
+          <Works />
+        </MotionSection>
+        <MotionSection>
+          {' '}
+          <Sustainability />
+        </MotionSection>
       </AppShell.Main>
     </>
   );
